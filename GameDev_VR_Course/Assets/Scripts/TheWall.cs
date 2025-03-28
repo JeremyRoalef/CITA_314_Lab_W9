2fbad7360d99bbb93a07ac18e5408b03fbdf4915
JeremyRoalef <roalef944@morrisville.edu>
2025-03-28 14:20:41 -0400

    Created procedural column generation function.

--- GameDev_VR_Course/Assets/Scripts/TheWall.cs
+++ GameDev_VR_Course/Assets/Scripts/TheWall.cs
@@ -7,10 +7,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class TheWall : MonoBehaviour
 {
     [SerializeField]
+    int columns;
+
+    [SerializeField]
+    int rows;
+
+    [SerializeField]
     GameObject wallCubePrefab;
 
     [SerializeField]
     GameObject socketWallCubePrefab;
+
+    [SerializeField]
+    int socketWallPosition = 1;
 
     [SerializeField]
     XRSocketInteractor wallSocket;
@@ -24,7 +33,8 @@ public class TheWall : MonoBehaviour
     Vector3 cubeSize;
     Vector3 spawnPos;
 
-    // Start is called before the first frame update
+
+
     void Start()
     {
         if (wallCubePrefab != null)
@@ -38,34 +48,10 @@ public class TheWall : MonoBehaviour
 
     void BuildWall()
     {
-        wallCubes = new GameObject[2];
-        if (wallCubePrefab != null)
+        for (int i = 0; i < columns; i++)
         {
-            wallCubes[0] = Instantiate(wallCubePrefab, spawnPos, transform.rotation);
-        }
-
-        spawnPos.y += cubeSize.y + cubeSpacing;
-
-        if (socketWallCubePrefab != null)
-        {
-
-            wallCubes[1] = Instantiate(socketWallCubePrefab, spawnPos, transform.rotation);
-
-            wallSocket = wallCubes[1].GetComponentInChildren<XRSocketInteractor>();
-
-            if (wallSocket != null)
-            {
-                wallSocket.selectEntered.AddListener(WallSocket_OnSelectEntered);
-                wallSocket.selectExited.AddListener(WallSocket_OnSelectExited);
-            }
-        }
-
-        for (int i = 0; i < wallCubes.Length; i++)
-        {
-            if (wallCubes[i] != null)
-            {
-                wallCubes[i].transform.SetParent(transform);
-            }
+            GenerateColumn(rows, true);
+            spawnPos.x += cubeSize.x + cubeSpacing;
         }
     }
 
@@ -91,15 +77,66 @@ public class TheWall : MonoBehaviour
         }
     }
 
+    void GenerateColumn(int height, bool socketable)
+    {
+        spawnPos.y = transform.position.y;
+        wallCubes = new GameObject[height];
+
+        for (int i = 0; i < wallCubes.Length; i++)
+        {
+            if (wallCubePrefab != null)
+            {
+                wallCubes[i] = Instantiate(wallCubePrefab, spawnPos, transform.rotation);
+                if (i== 0)
+                {
+                    wallCubes[i].name = "column";
+                    wallCubes[i].transform.SetParent(transform);
+                }
+                else
+                {
+                    wallCubes[i].transform.SetParent(wallCubes[0].transform);
+                }
+            }
+            
+            spawnPos.y += cubeSize.y + cubeSpacing;
+        }
+
+        if (socketable && socketWallCubePrefab != null)
+        {
+            if (socketWallPosition < 0 || socketWallPosition >= height)
+            {
+                socketWallPosition = 0;
+            }
+
+            if (wallCubes[socketWallPosition] != null)
+            {
+                Vector3 pos = wallCubes[socketWallPosition].transform.position;
+                DestroyImmediate(wallCubes[socketWallPosition]);
+
+                wallCubes[socketWallPosition] = Instantiate(socketWallCubePrefab, pos, transform.rotation);
+                if (socketWallPosition == 0)
+                {
+                    wallCubes[socketWallPosition].transform.SetParent(transform);
+                }
+                else
+                {
+                    wallCubes[socketWallPosition].transform.SetParent(wallCubes[0].transform);
+                }
+                
+                wallSocket = wallCubes[socketWallPosition].GetComponentInChildren<XRSocketInteractor>();
+
+                if (wallSocket != null)
+                {
+                    wallSocket.selectEntered.AddListener(WallSocket_OnSelectEntered);
+                    wallSocket.selectExited.AddListener(WallSocket_OnSelectExited);
+                }
+            }
+        }
+    }
+
     private void SetObjectToKinematic(GameObject obj, bool enableKinematic)
     {
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         rb.isKinematic = enableKinematic;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
